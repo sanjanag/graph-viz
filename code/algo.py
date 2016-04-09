5e5fd85ce30641106b24b536724df5b04deede6d
Amartya <amartya@cse.iitk.ac.in>
2016-04-09 12:46:12 +0530

    God Send

--- code/algo.py
+++ code/algo.py
@@ -10,7 +10,7 @@ def initialise_v():
     column_id INT NOT NULL,
     weight REAL NOT NULL,
     PRIMARY KEY(row_id,column_id));''')
-    gX.conn.commit()p
+    gX.conn.commit()
 
     cur.execute('''select count(*) from nodes;''')
     num_nodes =  cur.fetchone()[0]
@@ -46,3 +46,80 @@ def calc_Dinverse():
 def create_L():
     cur.execute("update A set similarity = similarity*(select value from D where D.id = A.row_id)")
     gX.conn.commit()
+
+def create_VT():
+    cur.execute('''CREATE TABLE IF NOT EXISTS VT
+    (row_id INT NOT NULL,
+    column_id INT NOT NULL,
+    weight REAL NOT NULL,
+    PRIMARY KEY(row_id,column_id));''')
+    gX.conn.commit()
+
+
+def calc_VT():
+    cur.execute("insert into VT (row_id,column_id,weight) select row_id, column_id, weight from (select A.row_id as row_id, V.column_id as column_id, sum(A.similarity*V.weight) as weight from A,V where A.column_id=V.row_id group by A.row_id,V.column_id) as X")
+    gX.conn.commit()
+    cur.execute("select sum(VT.weight*VT.weight) from VT")
+    norm = math.sqrt(cur.fetchone()[0])
+    cur.execute("update VT set weight = weight/(select sum(VT.weight*VT.weight) from VT)")
+    gX.conn.commit()
+    
+def create_delta():
+    cur.execute('''CREATE TABLE IF NOT EXISTS delta
+    (row_id INT NOT NULL,
+    column_id INT NOT NULL,
+    weight REAL NOT NULL,
+    PRIMARY KEY(row_id,column_id));''')
+    gX.conn.commit()
+
+    cur.execute('''select count(*) from nodes;''')
+    num_nodes =  cur.fetchone()[0]
+    for i in range(num_nodes):
+        cur.execute("INSERT INTO delta VALUES(%s, %s, %s)", (i+1, 1, 0))
+    gX.conn.commit()
+
+def create_deltaT():
+    cur.execute('''CREATE TABLE IF NOT EXISTS deltaT
+    (row_id INT NOT NULL,
+    column_id INT NOT NULL,
+    weight REAL NOT NULL,
+    PRIMARY KEY(row_id,column_id));''')
+    gX.conn.commit()
+
+def calc_deltaT():
+    cur.execute("insert into deltaT select VT.row_id, VT.column_id, abs(VT.weight-V.weight) as diff from VT, V where VT.row_id=V.row_id and VT.column_id= V.Column_id")
+
+
+
+    
+def iterate():
+    iter = 0
+    cur.execute("select count(*) from (select deltaT.row_id, deltaT.column_id, abs(deltaT.weight - delta.weight) as diff from deltaT, delta where deltaT.row_id=delta.row_id and deltaT.column_id=delta.column_id) as X where X.diff>.0001")
+    count = cur.fetchone()[0]
+    while(iter!=10 or count != 0):
+        cur.execute("drop table V")
+        gX.conn.commit()
+        cur.execute("alter table VT rename to V")
+        gX.conn.commit()
+        cur.execute("drop table delta")
+        gX.conn.commit()
+        cur.execute("alter table deltaT rename to delta")
+        gX.conn.commit()
+        create_VT()
+        calc_VT()
+        create_deltaT()
+        calc_deltaT()
+        gX.conn.commit()
+        iter += 1
+'''
+initialise_v()
+create_A()
+create_D()
+calc_Dinverse()
+create_L()
+create_VT()
+calc_VT()
+create_delta()
+create_deltaT()
+calc_deltaT()'''
+iterate()
